7f31d90ddec34fc2ebfe670a874c4b9cb8258a17
dhopp1 <daniel.hopp1@gmail.com>
2025-04-17 14:55:30 +0200

    Bug fix

--- llads/tooling.py
+++ llads/tooling.py
@@ -7,33 +7,39 @@ from operator import itemgetter
 def gen_tool_call(llm, tools, prompt):
     "bind tools to a custom LLM"
 
-    # render tools as a string    
+    # render tools as a string
     rendered_tools = render_text_description(tools)
-    
+
     system_prompt = f"""You are an assistant that has access to the following set of tools. Here are the names and descriptions for each tool:
 
     {rendered_tools}
 
     Given the user input, return the name and input of the tool to use. Return your response as a JSON blob with 'name' and 'arguments' keys and nothing else."""
 
+    # choosing tool call
     combined_prompt = ChatPromptTemplate.from_messages(
         [("system", system_prompt), ("user", "{input}")]
     )
-    
+
     tool_chain = combined_prompt | llm | JsonOutputParser()
-    
+
     try:
         tool_call = tool_chain.invoke({"input": prompt})
     except:
         tool_call = "error"
-        
-    if True:#try:
-        invoked_chain = tool_chain | itemgetter("arguments") | [_ for _ in tools if _.name == tool_call["name"]][0]
+
+    # actual running of tool
+    try:
+        invoked_chain = (
+            tool_chain
+            | itemgetter("arguments")
+            | [_ for _ in tools if _.name == tool_call["name"]][0]
+        )
         invoked_result = invoked_chain.invoke({"input": prompt})
-    else:#except:
+    except:
         invoked_chain = "error"
         invoked_result = "error"
-    
+
     return {
         "tool_chain": tool_chain,
         "tool_call": tool_call,
