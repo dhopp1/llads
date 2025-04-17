1b2e80d62ee52e7ace2aab09fe94627b4adb088e
dhopp1 <daniel.hopp1@gmail.com>
2025-04-17 16:00:23 +0200

    Adding option for multiple tool calls

--- llads/tooling.py
+++ llads/tooling.py
@@ -1,4 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain.tools.render import render_text_description
 from langchain_core.output_parsers import JsonOutputParser
 from operator import itemgetter
@@ -7,6 +8,11 @@ from operator import itemgetter
 def gen_tool_call(llm, tools, prompt):
     "bind tools to a custom LLM"
 
+    def tool_chain(model_output):
+        tool_map = {tool.name: tool for tool in tools}
+        chosen_tool = tool_map[model_output["name"]]
+        return itemgetter("arguments") | chosen_tool
+
     # render tools as a string
     rendered_tools = render_text_description(tools)
 
@@ -14,35 +20,34 @@ def gen_tool_call(llm, tools, prompt):
 
     {rendered_tools}
 
-    Given the user input, return the name and input of the tool to use. Return your response as a JSON blob with 'name' and 'arguments' keys and nothing else."""
+    Given the user input, return the name and input of the tool to use. Return your response as a JSON blob with 'name' and 'arguments' keys and nothing else. If you need multiple tools to answer the user's query, return a list of JSON blobs for each tool"""
 
     # choosing tool call
     combined_prompt = ChatPromptTemplate.from_messages(
         [("system", system_prompt), ("user", "{input}")]
     )
 
-    tool_chain = combined_prompt | llm | JsonOutputParser()
+    select_tool_chain = combined_prompt | llm | JsonOutputParser()
 
     try:
-        tool_call = tool_chain.invoke({"input": prompt})
+        tool_call = select_tool_chain.invoke({"input": prompt})
     except:
         tool_call = "error"
 
     # actual running of tool
-    try:
-        invoked_chain = (
-            tool_chain
-            | itemgetter("arguments")
-            | [_ for _ in tools if _.name == tool_call["name"]][0]
-        )
-        invoked_result = invoked_chain.invoke({"input": prompt})
-    except:
-        invoked_chain = "error"
-        invoked_result = "error"
+    if type(tool_call) != list:
+        tool_call = [tool_call]
+
+    invoked_results = []
+    for i in range(len(tool_call)):
+        tool_i = RunnableLambda(lambda args: tool_call[i]) | tool_chain
+
+        try:
+            invoked_results.append(tool_i.invoke(""))
+        except:
+            invoked_results = ["error"]
 
     return {
-        "tool_chain": tool_chain,
         "tool_call": tool_call,
-        "invoked_chain": invoked_chain,
-        "invoked_result": invoked_result,
+        "invoked_result": invoked_results,
     }
